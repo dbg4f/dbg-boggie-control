5d28c098c3b9a162fc41d6f0acbb9397e5abb003
dmitri.bogdel@gmail.com <dmitri.bogdel@gmail.com@d948064e-c27c-1d99-8f17-6be6a2cf1cc0>
2014-10-03 08:26:46 +0000

    Jetty WS prototype, additionally hosted fully  using FileServer

--- webapp/ws-test.js
+++ webapp/ws-test.js
@@ -1,4 +1,5 @@
-var ws = new WebSocket("ws://127.0.0.1:8080/");
+//var ws = new WebSocket("ws://127.0.0.1:9090/");
+var ws = new WebSocket("ws://" + location.host);
 
 ws.onopen = function() {
     alert("Opened!");
