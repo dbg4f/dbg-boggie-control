23e6ebc399228ee6a3c5a16083e832a7b88abf2e
dmitri.bogdel@gmail.com <dmitri.bogdel@gmail.com@d948064e-c27c-1d99-8f17-6be6a2cf1cc0>
2014-11-21 13:25:22 +0000

    caret position calculation in polar coordinates

--- trunk/src/dbg/misc/ws/PosCalc.java
+++ trunk/src/dbg/misc/ws/PosCalc.java
@@ -7,11 +7,8 @@ public class PosCalc {
 
   public static final Pair LEVER_LENGTHS = new Pair(87.0, 160.0);
 
-
   public static final Pair NEAR_CALIBRATION_POINT1 = new Pair(degreesToAngle(90.0), 0.3165); // degrees - sensor
   public static final Pair NEAR_CALIBRATION_POINT2 = new Pair(degreesToAngle(33.0), 0.179);
-
-  public static final double NEAR_CALIBRATION_POS = 0.179;   //
 
   public static final Pair FAR_CALIBRATION_POINT1 = new Pair(degreesToAngle(0.0), 0.1008);
   public static final Pair FAR_CALIBRATION_POINT2 = new Pair(degreesToAngle(46.0), 0.24435);
@@ -46,6 +43,35 @@ public class PosCalc {
     return new Pair(pointNearLever.near + pointFarLeverFromStart.near, pointNearLever.far + pointFarLeverFromStart.far);
   }
 
+  private static Pair calcPosTarget(Pair targetCaretPos) {
+
+    // TODO: check for max/min possible values
+
+
+    double a = LEVER_LENGTHS.far;
+    double b = LEVER_LENGTHS.near;
+
+    double polarR = Math.sqrt(targetCaretPos.near * targetCaretPos.near + targetCaretPos.far * targetCaretPos.far);
+    double polarA = StrictMath.atan(targetCaretPos.far / targetCaretPos.near);
+
+    double c = polarR;
+
+    double alpha = Math.acos((b * b + c * c - a * a) / (2.0 * b * c));
+
+    double beta = Math.acos((a * a + c * c - b * b) / (2.0 * a * c));
+
+
+    double targetAngleNear = alpha + polarA;
+    double targetAngleFar = -beta;
+
+    double targetPosNear = interpolateFindFar(targetAngleNear, NEAR_CALIBRATION_POINT1, NEAR_CALIBRATION_POINT2);
+    double targetPosFar = interpolateFindFar(targetAngleFar, FAR_CALIBRATION_POINT1, FAR_CALIBRATION_POINT2);
+
+    return new Pair(targetPosNear, targetPosFar);
+
+  }
+
+
   private static double interpolateFindNear(double far, Pair calibrationPair1, Pair calibrationPair2) {
       // y = k*x + c
       // k = tg(alpha)
@@ -60,6 +86,20 @@ public class PosCalc {
 
   }
 
+  private static double interpolateFindFar(double near, Pair calibrationPair1, Pair calibrationPair2) {
+      // y = k*x + c
+      // k = tg(alpha)
+      // tg(a) = dy/dx
+      // c = y - k*x
+      // x = (y - c) / k
+
+    double k = (calibrationPair2.far - calibrationPair1.far) / (calibrationPair2.near - calibrationPair1.near);
+    double c = calibrationPair1.far - k * calibrationPair1.near;
+
+    return k * near + c;
+
+  }
+
   private static double degreesToAngle(double degrees) {
     return (2.0*Math.PI / 360.0) * degrees;
   }
@@ -69,6 +109,19 @@ public class PosCalc {
   }
 
 
+  public static void main(String[] args) {
+
+    Pair p = calcPosTarget(new Pair(235.9, 12.55));
+
+    System.out.println("p = " + p);
+
+    p = calcCaretPos(p);
+
+    System.out.println("p = " + p);
+
+
+
+  }
 
 
 
