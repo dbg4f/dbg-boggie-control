2b00d2ff2dd62eea53438b45e47fc169e3a76dbe
dmitri.bogdel@gmail.com <dmitri.bogdel@gmail.com@d948064e-c27c-1d99-8f17-6be6a2cf1cc0>
2014-12-01 16:33:03 +0000

    Vector2D and enhancements in Line

--- src/dbg/misc/calc/LinearDependency.java
+++ src/dbg/misc/calc/LinearDependency.java
@@ -2,12 +2,28 @@ package dbg.misc.calc;
 
 public class LinearDependency {
 
+    class NormalForm {
+
+      // Ax + By + C = 0
+
+      public final double A,B,C;
+
+      NormalForm() {
+        A = k;
+        B = -1;
+        C = b;
+      }
+
+    }
+
     public final double k;
     public final double b;
+    private final NormalForm normalForm;
 
     public LinearDependency(double k, double b) {
         this.k = k;
         this.b = b;
+        normalForm = new NormalForm();
     }
 
     public LinearDependency (CartesianPoint p1, CartesianPoint p2) {
@@ -18,14 +34,32 @@ public class LinearDependency {
 
         k = (p2.y - p1.y) / (p2.x - p1.x);
         b = p1.y - k * p1.x;
-
+        normalForm = new NormalForm();
     }
 
     public double getY(double x) {
         return k * x + b;
     }
 
+    public NormalForm toNormalForm() {
+      return normalForm;
+    }
 
+    public Vector2D normalVector() {
+      return new Vector2D(normalForm.A, normalForm.B);
+    }
+
+  /*
+    public boolean isParallel(LinearDependency line2) {
+
+    }
+
+    public CartesianPoint intersect(LinearDependency line2) {
+
+
+
+    }
+    */
     @Override
     public String toString() {
         return "LinearDependency{" +
