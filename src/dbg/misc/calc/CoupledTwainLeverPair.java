4efbf442043f50ac0a878753ebc7b86de22d7736
dbg4f <oismae3718>
2015-05-15 18:06:01 +0300

    levers modeling, svg output

--- src/dbg/misc/calc/CoupledTwainLeverPair.java
+++ src/dbg/misc/calc/CoupledTwainLeverPair.java
@@ -4,8 +4,8 @@ package dbg.misc.calc;
 
 public class CoupledTwainLeverPair {
 
-    public final CartesianPoint leverLeftStart = new CartesianPoint(0, 100/2-12);
-    public final CartesianPoint leverRightStart = new CartesianPoint(0, 100/2+12);
+    public final CartesianPoint leverLeftStart = new CartesianPoint(100/2-12, 0);
+    public final CartesianPoint leverRightStart = new CartesianPoint(100/2+12, 0);
     public final Rectangle workingArea = new Rectangle(0, 75, 100, 125);
 
 
@@ -18,4 +18,18 @@ public class CoupledTwainLeverPair {
     public final double penToRightUpperBottomDistance = 75;
 
 
+    @Override
+    public String toString() {
+        return "CoupledTwainLeverPair{" +
+                "leverLeftStart=" + leverLeftStart +
+                ", leverRightStart=" + leverRightStart +
+                ", workingArea=" + workingArea +
+                ", leverLeftLowerLength=" + leverLeftLowerLength +
+                ", leverRightLowerLength=" + leverRightLowerLength +
+                ", leverLeftUpperLength=" + leverLeftUpperLength +
+                ", leverRightUpperLength=" + leverRightUpperLength +
+                ", penToRightUpperTopDistance=" + penToRightUpperTopDistance +
+                ", penToRightUpperBottomDistance=" + penToRightUpperBottomDistance +
+                '}';
+    }
 }
--- src/dbg/misc/calc/LeversPosition.java
+++ src/dbg/misc/calc/LeversPosition.java
@@ -6,7 +6,10 @@ public class LeversPosition {
 
     private final CoupledTwainLeverPair pair;
 
+    CartesianPoint pen;
     CartesianPoint rightLowerTop;
+    CartesianPoint bothTop;
+    CartesianPoint leftLowerTop;
 
     public LeversPosition(CoupledTwainLeverPair pair) {
         this.pair = pair;
@@ -14,17 +17,67 @@ public class LeversPosition {
 
     public LeverAngles calcAngles(CartesianPoint pen) {
 
+        this.pen = pen;
+
         rightLowerTop = new CircleIntersection(
-                new Circle(pair.penToRightUpperTopDistance, pen),
+                new Circle(pair.penToRightUpperBottomDistance, pen),
                 new Circle(pair.leverRightLowerLength, pair.leverRightStart)).getRight();
 
 
+        bothTop = new CircleIntersection(
+                new Circle(pair.penToRightUpperTopDistance, pen),
+                new Circle(pair.leverRightUpperLength, rightLowerTop)).getLeft();
 
+        leftLowerTop = new CircleIntersection(
+                new Circle(pair.leverLeftUpperLength, bothTop),
+                new Circle(pair.leverLeftLowerLength, pair.leverLeftStart)).getLeft();
 
         return new LeverAngles(0, 0);
     }
 
+    @Override
+    public String toString() {
+        return "LeversPosition{" +
+                "pair=" + pair +
+                ", rightLowerTop=" + rightLowerTop +
+                ", bothTop=" + bothTop +
+                '}';
+    }
 
+
+    public static String draw(CartesianPoint p1, CartesianPoint p2) {
+        StringBuffer buffer = new StringBuffer();
+
+        buffer.append(String.format("ctx.moveTo(%s,%s);\n", 3*Math.round(p1.x) + 300, -Math.round(p1.y)*3 + 400));
+        buffer.append(String.format("ctx.lineTo(%s,%s);\n", 3*Math.round(p2.x) + 300, -Math.round(p2.y)*3 + 400));
+        buffer.append("ctx.stroke();\n");
+
+        return buffer.toString();
+    }
+
+    public static void main(String[] args) {
+
+        CoupledTwainLeverPair pair = new CoupledTwainLeverPair();
+        LeversPosition pos = new LeversPosition(pair);
+
+        pos.calcAngles(new CartesianPoint(70, 100));
+
+        System.out.println("pos = " + pos);
+        System.out.println("pen             = " + pos.pen);
+        System.out.println("rightLowerTop   = " + pos.rightLowerTop);
+        System.out.println("bothTop         = " + pos.bothTop);
+        System.out.println("leftLowerTop    = " + pos.leftLowerTop);
+
+
+        System.out.println(draw(pair.leverRightStart, pos.rightLowerTop));
+        System.out.println(draw(pos.rightLowerTop,  pos.pen));
+        System.out.println(draw(pos.bothTop,        pos.pen));
+        System.out.println(draw(pos.bothTop,        pos.rightLowerTop));
+
+        System.out.println(draw(pair.leverLeftStart,  pos.leftLowerTop));
+        System.out.println(draw(pos.leftLowerTop,     pos.bothTop));
+
+    }
 
 
 }
