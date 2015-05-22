9d2bacbe93f20a8510ba81f0ba18da4979e456ad
dbg4f <dmitri.bogdel@gmail.com>
2015-05-22 22:54:59 +0300

    controller skeleton

---
+++ src/dbg/misc/calc/drive/CncCommand.java
@@ -0,0 +1,13 @@
+package dbg.misc.calc.drive;
+
+public class CncCommand {
+
+    public static CncCommand moveTo(int x, int y) {
+        return new CncCommand();
+    }
+
+    public static CncCommand lineTo(int x, int y) {
+        return new CncCommand();
+    }
+
+}
